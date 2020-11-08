efbcd0d9f2bce8ce2c1b6f261284d410839ae763
Jun Shawn <wwwsean13579@gmail.com>
2020-11-08 16:25:12 +0800

    更新

--- index.js
+++ index.js
@@ -52,15 +52,12 @@ setTimeout(function(){
 },1650);
 
 $(function() {
-    /* 按下GoTop按鈕時的事件 */
     $('#gotop').click(function(){
         $('html,body').animate({ scrollTop: 0 }, 'slow');   /* 返回到最頂上 */
         return false;
     });
-    
-    /* 偵測卷軸滑動時，往下滑超過400px就讓GoTop按鈕出現 */
     $(window).scroll(function() {
-        if ( $(this).scrollTop() > 500){
+        if ( $(this).scrollTop() > 1000){
             $('#gotop').fadeIn(1000);
         } else {
             $('#gotop').fadeOut(500);
